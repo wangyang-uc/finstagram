11f8e24757849583c760579771f399222d0d3621
Alex Wang <wangyang.uc@gmail.com>
2017-09-28 02:23:43 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,25 @@ get '/' do
   @posts = Post.order(created_at: :desc)
   erb(:index)
 end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
+end
