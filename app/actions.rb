f555bdf317ca765cc7965e45207185414af50aab
Alex Wang <wangyang.uc@gmail.com>
2017-10-09 22:22:43 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @posts = Post.order(created_at: :desc)
   erb(:index)
@@ -8,8 +14,6 @@ get '/signup' do     # if a user navigates to the path "/signup",
   erb(:signup)       # render "app/views/signup.erb"
 end
 
-
-
 post '/signup' do
   email      = params[:email]
   avatar_url = params[:avatar_url]
@@ -19,8 +23,55 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
+end
+
+get '/login' do    # when a GET request comes into /login
+  erb(:login)      # render app/views/login.erb
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+
+get '/post' do
+  @post = Post.new
+  erb(:"post/new")
+end
+
+post '/post' do
+  photo_url = params[:photo_url]
+
+  @post = Post.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @post.save
+    redirect(to('/'))
+  else
+    erb(:"post/new")
+  end
+end
+
+get '/post/:id' do
+  @post = Post.find(params[:id])   # find the post with the ID from the URL
+  erb(:"post/show")               # render app/views/posts/show.erb
 end
