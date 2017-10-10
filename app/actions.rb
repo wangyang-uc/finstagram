365d4417480712570dc9cd392c1465417e696ca3
Alex Wang <wangyang.uc@gmail.com>
2017-10-10 02:18:33 +0000

    W6D1v2

--- app/actions.rb
+++ app/actions.rb
@@ -75,3 +75,33 @@ get '/post/:id' do
   @post = Post.find(params[:id])   # find the post with the ID from the URL
   erb(:"post/show")               # render app/views/posts/show.erb
 end
+
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  post_id = params[:post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, post_id: post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
+
+post '/likes' do
+  post_id = params[:post_id]
+
+  like = Like.new({ post_id: post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
