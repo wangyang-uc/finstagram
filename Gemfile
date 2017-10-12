b7d7aca67b5d2c68d435f97b54d099eac637358a
Alex Wang <wangyang.uc@gmail.com>
2017-10-12 01:48:03 +0000

    Final

--- Gemfile
+++ Gemfile
@@ -16,3 +16,8 @@ group :development, :test do
   gem 'shotgun'
   gem 'sqlite3'
 end
+group :production do
+  gem 'rails_12factor'
+  gem 'pg'
+end
+
--- config/database.rb
+++ config/database.rb
@@ -2,12 +2,22 @@ configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+    set :database, {
+      adapter: "postgresql",
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
